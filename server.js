14bbd5a4c63c9c53a85f2ad5b8c7c9b7f3381012
Tavo39 <gtaquezj@miumg.edu.gt>
2026-05-28 01:33:40 -0600

    Actualizar Servidor

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+// Configurar la carpeta "public" para servir archivos estáticos (HTML, CSS)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta principal que cargará automáticamente tu index.html
 app.get('/', (req, res) => {
-    res.json({
-        status: "success",
-        message: "¡Infraestructura y Contenedor funcionando perfectamente!",
-        timestamp: new Date(),
-        environment: "Local (Simulación Cloud)"
-    });
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+    console.log(`Servidor web corriendo en el puerto ${PORT}`);
 });
